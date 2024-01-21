aac86b41a60c68790fdf56c0551ed92d82f9651b
Fabian Ochoa <cafaruoc2000@hotmail.com>
2024-01-21 11:24:21 -0500

    .

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,59 +1,14 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-
-const spotifyAuthUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${
-  import.meta.env.VITE_SPOTIFY_CLIENT_ID
-}&redirect_uri=${
-  import.meta.env.VITE_SPOTIFY_REDIRECT_URI
-}&scope=user-read-currently-playing+user-read-playback-state`;
-
-const handleSpotifyAuth = () => {
-  window.location.href = spotifyAuthUrl;
-};
+import useSpotifyAuth from "./useSpotifyAuth";
 
 export default function useFetch() {
-  const [accessToken, setAccessToken] = useState("");
   const [currentSongData, setCurrentSongData] = useState(null);
-  const [expiresIn, setExpiresIn] = useState(0);
   const [plabackStateData, setPlaybackStateData] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const location = useLocation();
+  const { handleSpotifyAuth } = useSpotifyAuth();
 
-  useEffect(() => {
-    const getAccessTokenFromStorage = () => {
-      const storedToken = localStorage.getItem("spotifyAccessToken");
-      if (storedToken) {
-        setAccessToken(storedToken);
-      }
-    };
-
-    const saveAccessTokenToStorage = (token) => {
-      localStorage.setItem("spotifyAccessToken", token);
-    };
-
-    const getAccessTokenFromURL = () => {
-      const hashParams = location.hash
-        .substring(1)
-        .split("&")
-        .reduce((acc, param) => {
-          const [key, value] = param.split("=");
-          acc[key] = decodeURIComponent(value);
-          return acc;
-        }, {});
-
-      if (hashParams.access_token && hashParams.expires_in) {
-        setAccessToken(hashParams.access_token);
-        setExpiresIn(hashParams.expires_in);
-        window.location.href = window.location.origin;
-        saveAccessTokenToStorage(hashParams.access_token);
-      } else {
-        getAccessTokenFromStorage();
-      }
-    };
-
-    getAccessTokenFromURL();
-  }, [location, accessToken]);
+  const accessToken = localStorage.getItem("spotifyAccessToken");
 
   useEffect(() => {
     const headers = new Headers();
@@ -92,14 +47,8 @@ export default function useFetch() {
         if (response.ok) {
           const data = await response.json();
           setCurrentSongData(data);
-          //   console.log(data);
-          if (expiresIn > 0) {
-            setTimeout(() => {
-              handleSpotifyAuth();
-            }, expiresIn * 1000 - 60000);
-          }
-        } else {
-          throw new Error(`Error fetching current song: ${response.status}`);
+        } else if (response.status === 401) {
+          handleSpotifyAuth();
         }
       } catch (error) {
         console.error(error);
@@ -116,7 +65,7 @@ export default function useFetch() {
 
       return () => clearInterval(intervalId);
     }
-  }, [accessToken, expiresIn]);
+  });
 
   return {
     accessToken,
